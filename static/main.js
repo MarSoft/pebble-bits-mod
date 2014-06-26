51e73ee9e283bc9432dab8b1d4672687f44233b2
Semyon Maryasin <simeon@maryasin.name>
2014-06-27 03:06:36 +0400

    Full support for dynamic loading of patch list

--- static/main.js
+++ static/main.js
@@ -1,8 +1,17 @@
 $(function() {
 	$("#version_form").submit(function() {
 		ver = $("#fw_ver").val();
-		code = $.ajax("/patches?ver="+ver);
-		$("#patches").html(code);
+		$("#patches").text("Loading...");
+		$.ajax("/patches?ver="+ver)
+		.done(function(code) {
+			if(code)
+				$("#patches").html(code);
+			else
+				$("#patches").html("No compatible patches for this version");
+		})
+		.fail(function(x, s) {
+			$("#patches").text("Error: "+s);
+		});
 
 		return false;
 	});
