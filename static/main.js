b7c12a5585b83f8a9b070af4a96a306dd1e1fe68
Semyon Maryasin <simeon@maryasin.name>
2014-06-27 15:04:05 +0400

    Moved "no patches/no options" logic to templates

--- static/main.js
+++ static/main.js
@@ -4,35 +4,27 @@ $(function() {
 		$("#patches").text("Loading...");
 		$.ajax("/patches?ver="+ver)
 		.done(function(code) {
-			if(code) {
-				$("#patches").html(code);
-				$(".patch_box").change(function(e) {
-					id = this.id;
-					opts = "#"+id+"_options";
-					if($(this).is(":checked")) {
-						if(!$(opts).is(".loaded")) {
-							$.ajax("/options?patch="+id)
-							.done(function(code) {
-								if(code)
-									$(opts).html(code);
-								else
-									$(opts).text("No options");
-								$(opts).addClass("loaded");
-							})
-							.fail(function(x,y,s) {
-								$(opts).text("Error: "+s);
-							});
-						}
-						$(opts).show();
-					} else {
-						$(opts).hide();
+			$("#patches").html(code);
+			$(".patch_box").change(function(e) {
+				id = this.id;
+				opts = "#"+id+"_options";
+				if($(this).is(":checked")) {
+					if(!$(opts).is(".loaded")) {
+						$.ajax("/options?patch="+id)
+						.done(function(code) {
+							$(opts).html(code);
+							$(opts).addClass("loaded");
+						})
+						.fail(function(x,y,s) {
+							$(opts).text("Error: "+s);
+						});
 					}
-				});
-				$("#gen_btn").removeAttr("disabled");
-			} else {
-				$("#patches").html("No compatible patches for this version");
-				$("#gen_btn").addAttr("disabled");
-			}
+					$(opts).show();
+				} else {
+					$(opts).hide();
+				}
+			});
+			$("#gen_btn").removeAttr("disabled");
 		})
 		.fail(function(x,y,s) {
 			$("#patches").text("Error: "+s);
